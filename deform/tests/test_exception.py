d13d5194e3f9257a3597627964866d6cd4d68a1b
Atsushi Odagiri <aodagx@gmail.com>
2011-07-10 19:52:30 +0900

    garden

--- deform/tests/test_exception.py
+++ deform/tests/test_exception.py
@@ -13,10 +13,21 @@ class TestValidationFailure(unittest.TestCase):
         result = e.render()
         self.assertEqual(result, cstruct)
 
+    def test_get_widget_resources(self):
+        widget = DummyWidget()
+        form = DummyForm(widget)
+        form.widget_resources = {"js": "this is resources"}
+        cstruct = {}
+        e = self._makeOne(form, cstruct, None)
+        resources = e.get_widget_resources()
+        self.assertEqual(resources, {"js": "this is resources"})
+
 class DummyForm(object):
     def __init__(self, widget):
         self.widget = widget
-    
+    def get_widget_resources(self):
+        return self.widget_resources
+
 class DummyWidget(object):
     def serialize(self, field, cstruct):
         return cstruct
